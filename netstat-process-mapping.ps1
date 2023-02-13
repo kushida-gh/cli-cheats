29d4f6b829c572d0b92a16d1829b78b5de670da4
Kimi Ushida <54966265+kushida-gh@users.noreply.github.com>
2023-02-12 22:10:07 -0800

    Update netstat-process-mapping.ps1

--- netstat-process-mapping.ps1
+++ netstat-process-mapping.ps1
@@ -5,4 +5,6 @@
 
 Get-Date -Format o | Out-File -Encoding Ascii -append footest.txt
 
-Get-NetTCPConnection -State Established | Select-Object -Property LocalPort,RemoteAddress,RemotePort,State,@{name='ProcessName ';expression={(Get-Process -Id $_.OwningProcess).Path}} | where {$_.RemoteAddress -ne "::1"} | where {$_.RemoteAddress -ne "127.0.0.1"} | Format-Table | Out-File -Encoding Ascii -append footest.txt
+# Get-NetTCPConnection -State Established | Select-Object -Property LocalPort,RemoteAddress,RemotePort,State,@{name='ProcessName ';expression={(Get-Process -Id $_.OwningProcess).Path}} | where {$_.RemoteAddress -ne "::1"} | where {$_.RemoteAddress -ne "127.0.0.1"} | Format-Table | Out-File -Encoding Ascii -append tcp-connects-established-and-their-processes.txt
+
+Get-NetTCPConnection -State Established | Select-Object LocalPort,RemoteAddress,RemotePort,State,OwningProcess,@{Name='cmdline';expression={(Get-WmiObject Win32_Process -filter "ProcessId = $($_.OwningProcess)").commandline}} | where {$_.RemoteAddress -ne "::1"} | where {$_.RemoteAddress -ne "127.0.0.1"} | Format-Table | Out-File -Encoding Ascii -append tcp-connects-established-and-their-processes.txt
