ec939591e8256a141547703ee36cdf01bbc9c41f
Kimi Ushida <54966265+kushida-gh@users.noreply.github.com>
2023-02-12 22:15:50 -0800

    Update netstat-process-mapping.ps1

--- netstat-process-mapping.ps1
+++ netstat-process-mapping.ps1
@@ -3,7 +3,7 @@
 # to capture what processes and related executable paths made connections to other remote hosts
 
 
-Get-Date -Format o | Out-File -Encoding Ascii -append footest.txt
+Get-Date -Format o | Out-File -Encoding Ascii -append  tcp-connects-established-and-their-processes.txt
 
 # Get-NetTCPConnection -State Established | Select-Object -Property LocalPort,RemoteAddress,RemotePort,State,@{name='ProcessName ';expression={(Get-Process -Id $_.OwningProcess).Path}} | where {$_.RemoteAddress -ne "::1"} | where {$_.RemoteAddress -ne "127.0.0.1"} | Format-Table | Out-File -Encoding Ascii -append tcp-connects-established-and-their-processes.txt
 
